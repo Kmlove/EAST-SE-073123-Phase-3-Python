a4ee1e56c1ef4122e3e141d0809cbc5bf7a50ea7
Kim Love <kimberlymlove15@gmail.com>
2023-09-17 22:00:17 -0400

    Demo complete

--- lib/cat.py
+++ lib/cat.py
@@ -1,8 +1,6 @@
-#✅ 5. Import the pet and cat class to use in debug.py
-#✅ 6. See pet.py
-
 #✅ 7. Create a subclass of Pet called Cat
 #✅ 7a. Import Pet from lib.pet
+from lib.pet import *
 
 #✅ 7b. Create an __init__ method that has all parameters of Pet including an additional parameter: indoor
 # self.name = name 
@@ -11,7 +9,7 @@
 # self.temperament = temperament
 # self.image_url = image_url
 
-#✅ 7c. Use super to pass Pet parameters to super class
+#✅ 7c. Use super to pass Pet parameters to sub class
 
 #✅ 7d. Update the instance in debug.py to rose = Cat('rose', 11, 'domestic longhair', 'sweet', 'rose.jpg', True)
 
@@ -21,3 +19,15 @@
 #✅ 10. Create a method called print_pet_details, to match the print_pet_details in Pet    
 
 #✅ 10a. Print the indoor attribute
+
+class Cat(Pet):
+    def __init__(self, name, age, breed, temperament, image_url, indoor):
+        super().__init__(name, age, breed, temperament, image_url)
+        self.indoor = indoor
+
+    def say_meow(self):
+        print("Meow")
+
+    def print_pet_details(self):
+        super().print_pet_details()
+        print(f"indoor: {self.indoor}")
--- lib/pet.py
+++ lib/pet.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # Class Attributes and Methods 
 
-#✅ 5. Import the pet and cat class to use in debug.py
-
 #✅ 6. Keep track of the total number of Pets created
 #✅ 6a. Create a class attribute
 
@@ -10,3 +8,28 @@
 #Pet.total_pets += 1
 
 #✅ 6c. Create a class method increase_pets that will increment total_pets
+
+class Pet:
+    total_pets = 0
+
+    def __init__(self, name, age, breed, temperament, image_url):
+        self.name = name
+        self.age = age
+        self.breed = breed
+        self.temperament = temperament
+        self.image_url = image_url
+        Pet.increase_pets()
+
+    @classmethod
+    def increase_pets(cls):
+        cls.total_pets += 1
+
+    def print_pet_details(self):
+        print(f'''
+            name: {self.name}
+            age: {self.age}
+            breed: {self.breed}
+            temperament: {self.temperament}
+            image_url: {self.image_url}
+        ''')
+
